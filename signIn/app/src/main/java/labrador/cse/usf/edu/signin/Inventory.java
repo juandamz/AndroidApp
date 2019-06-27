2fe82390a31c59353598ff3deb5a2cbc92497a8d
Camilo Sarabia <csarabia@mail.usf.edu>
2019-06-26 23:07:55 -0400

    aaa

--- signIn/app/src/main/java/labrador/cse/usf/edu/signin/Inventory.java
+++ signIn/app/src/main/java/labrador/cse/usf/edu/signin/Inventory.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -47,6 +48,35 @@ public class Inventory extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_search:openSearch();
+                return true;
+            case R.id.action_settings:openSettings();
+                return true;
+
+            case R.id.action_logout:
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+                return true;
+            default:return super.onOptionsItemSelected(item);
+        }
+
+
+
+    }
+
+
+
+    public void openSearch() {}
+    public void openSettings() {
+
     }
 
 }
